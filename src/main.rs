38a878afcce50dfa6f9d3de60e9a3eac4d6a3cba
Tabi3 <viper0998975@gmail.com>
2022-05-05 19:49:40 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,85 @@
-use serenity::{
-    async_trait,
-    client::{*, self},
-    framework::standard::{macros::{group, hook, command}, *},
-    http::Http,
-    model::{*, channel::Message},
-    prelude::*,
-    utils::*,
+use std::env;
+
+use serenity::async_trait;
+use serenity::builder::CreateInteractionResponseData;
+use serenity::model::channel::Embed;
+use serenity::model::gateway::Ready;
+use serenity::model::id::{ChannelId, GuildId};
+use serenity::model::interactions::application_command::{
+    ApplicationCommand, ApplicationCommandInteractionDataOptionValue, ApplicationCommandOptionType,
 };
-mod test_module;
-use test_module::*;
+use serenity::model::interactions::{ping, Interaction, InteractionResponseType};
+use serenity::prelude::*;
 
-use std::{collections::*, env, fmt::Write, sync::Arc};
-
-use tokio::sync::Mutex;
-struct ShardManagerContainer;
 struct Handler;
-struct CommandCounter;
 
-impl TypeMapKey for ShardManagerContainer {
-    type Value = Arc<Mutex<bridge::gateway::ShardManager>>;
+fn ping_func<'b, 'c>(
+    message: &'b mut CreateInteractionResponseData<'c>,
+) -> &'b mut CreateInteractionResponseData<'c> {
+    return message.embed(|e| {
+        e.field("Ping?", "Pong!", true)
+            .field("Ping-Pong Result", "Success", true)
+    });
 }
 
-impl TypeMapKey for CommandCounter {
-    type Value = HashMap<String, u64>;
+fn not_impl_func<'b, 'c>(
+    message: &'b mut CreateInteractionResponseData<'c>,
+) -> &'b mut CreateInteractionResponseData<'c> {
+    return message.content("not implemented :(");
 }
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _: Context, ready: prelude::Ready) {
-        print!("{} is Running\n---------------\n", ready.user.name);
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let content = match command.data.name.as_str() {
+                "ping" => ping_func,
+                _ => not_impl_func,
+            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(&content)
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        let guild_id = GuildId(906461867310977075);
+
+        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+            commands.create_application_command(|command| {
+                command.name("ping").description("A ping command")
+            })
+        })
+        .await;
     }
 }
-
-#[group]
-#[description = "A group of Math commands"]
-#[prefixes(math, m)]
-#[commands(anroot)]
-struct MathCommands;
-
-#[hook]
-async fn before(ctx: &Context, _msg: &Message, command_name: &str) -> bool {
-    print!("user {} called command {}\n",_msg.author.name ,command_name);
-    
-    let mut data = ctx.data.write().await;
-    let counter = data.get_mut::<CommandCounter>().expect("Weird shit");
-    let entry = counter.entry(command_name.to_string()).or_insert(0);
-    *entry += 1;
-
-    
-    return true;
-}
-
-#[hook]
-async fn after(_ctx: &Context, _msg: &Message, command_name: &str, command_result: CommandResult) {
-    match command_result {
-        Ok(()) => println!("Command '{}' was succesful", command_name),
-        Err(why) => println!("Command '{}' returned error {:?}", command_name, why),
-    }
-}
-
 
 #[tokio::main]
 async fn main() {
-    let token = "ODQxMzYyMDg2NjY2OTYwOTI2.YJlpgQ.IlqXgMo4LNP0RIyycxURdu5voZ8";
-    let http = Http::new(&token);
-    let (owner_id, bot_id) = match http.get_current_application_info().await {
-        Ok(info) => {
-            let mut owners = HashSet::new();
-            if let Some(team) = info.team {
-                owners.insert(team.owner_user_id);
-            } else {
-                owners.insert(info.owner.id);
-            }
-            match http.get_current_user().await {
-                Ok(bot_id) => (owners, bot_id.id),
-                Err(why) => panic!("Could not access the bot id: {:?}", why),
-            }
-        }
-        Err(why) => panic!("Couldn't access application info {:?}", why),
-    };
-    let framework = StandardFramework::new()
-        .configure(|c| {
-            c.with_whitespace(true)
-                .on_mention(Some(bot_id))
-                .prefix(".")
-                .delimiters(vec![", ", ","])
-                .owners(owner_id)
-        })
-        .before(before)
-        .after(after)
-        .group(&MATHCOMMANDS_GROUP);
+    // Configure the client with your Discord bot token in the environment.
+    let token = "ODQxMzYyMDg2NjY2OTYwOTI2.YJlpgQ.4w3mHvPHOYxwMKJwCDQh3eg2WQ8";
 
-    let mut client = Client::builder(token, GatewayIntents::all())
-                        .event_handler(Handler).framework(framework)
-                        .type_map_insert::<CommandCounter>(HashMap::default())
-                        .await.expect("Something went wrong creating client");
+    // Build our client.
+    let mut client = Client::builder(token, GatewayIntents::empty())
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
-}
-
-
-#[command]
-async fn anroot(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    let arg1 = args.single::<f64>()?;
-    let arg2 = args.single::<f64>()?;
-    msg.channel_id.say(&ctx.http, format!("{}", nroot(arg1, arg2, 1e-1))).await?;
-
-    Ok(())
 }
